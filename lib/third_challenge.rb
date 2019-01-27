f98eba8cdfa750de4a3e93dc81d094a5bad2206d
Mark Kanko <mkanko66@gmail.com>
2019-01-27 03:05:31 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,10 +19,12 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Ca[ulet",
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
